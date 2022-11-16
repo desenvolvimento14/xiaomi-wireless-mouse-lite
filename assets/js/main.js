cba8401a9b8b182b37c474138071d82d131e6f40
desenvolvimento14 <desenvolvimento14@dl.com.br>
2022-11-16 16:40:22 -0300

    Adjusting Landing Page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".slider").slick({
+        arrows: false,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});    
